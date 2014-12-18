4899233538407acf2b73ddbaac2b022d35fcb60f
vinz243 <vinz243>
2014-12-18 21:17:59 +0100

    Add basic functionnality and cmake file

---
+++ lib/base.h
@@ -0,0 +1,27 @@
+#ifndef BASE_H
+#define BASE_H
+
+
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreLogManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
+
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
+#include <SdkTrays.h>
+#include <SdkCameraMan.h>
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#endif
